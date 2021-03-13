6caf4dc146f2756551bebb1e979ab22b80bf4f28
AdityaTri03 <80306215+AdityaTri03@users.noreply.github.com>
2021-03-13 09:08:15 +0700

    Latihan Pertemuan 2

---
+++ Pertemuan2/Contoh.php
@@ -0,0 +1,10 @@
+<?php 
+$teman = "erik";
+if($teman == "geri"){
+	echo "Geri adalah teman saya";
+}elseif($teman == "erik"){
+	echo "Erik adalah teman saya";
+}else{
+	echo "Saya tidak punya teman";
+}
+?>
