1184ab55da242d375682ed16933e1d6b09bc5c39
AdityaTri03 <80306215+AdityaTri03@users.noreply.github.com>
2021-03-13 18:00:22 +0700

    Latihan Pertemuan 3

---
+++ Pertemuan3/Pert3_latihan3.php
@@ -0,0 +1,19 @@
+<?php
+$destination="Pamulang";
+Switch ($destination){
+case "Bandung":
+echo "Beli Cilok";
+break;
+case "Sukabumi":
+echo "Beli Bakso";
+break;
+case "Pamulang":
+echo "Beli Mie Ayam";
+break;
+case "Tokyo":
+echo "Bring lots of money";
+break;
+case "Caribbean Islands":
+echo "Bring a swimsuit";
+break;
+}
